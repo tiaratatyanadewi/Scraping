c8bd7db8c26f40f647c12026e31a9a7ea1fab197
tiaratatyanadewi <tiara.tatyanadewi.benjamin-2021@ftmm.unair.ac.id>
2024-05-26 18:26:10 +0700

    Initial commit

---
+++ bookscraper/bookscraper/items.py
@@ -0,0 +1,14 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+from scrapy import Field
+
+class BookscraperItem(scrapy.Item):
+    title = Field()
+    price = Field()
+    upc = Field()
+    image_url = Field()
+    url = Field()
